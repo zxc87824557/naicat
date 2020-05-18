4a39a9dbf90b1124afad35b8e6d5a45a4523c980
zxc87824557 <zxc90919zxc@gmail.com>
2020-05-18 13:12:16 +0800

    myvue

---
+++ myvue/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h1>{{text}}</h1>
+    <div class="cards">
+    <Card v-for="(card, index) in cards" :key="index" :data="card"></Card>
+    </div>
+  </div>
+</template>
+
+<script>
+// import元件名稱from路徑
+import Card from './components/card.vue'
+export default {
+  data () {
+    return {
+      text: 'aaa',
+      cards: [
+        {
+          img: 'https://dinbendon.kento520.tw/assets/img/res/pic/1.jpg',
+          title: '太師傅'
+        },
+        {
+          img: 'https://dinbendon.kento520.tw/assets/img/res/pic/2.jpg',
+          title: '吉野烤肉飯'
+        },
+        {
+          img: 'https://dinbendon.kento520.tw/assets/img/res/pic/3.jpg',
+          title: '烤飯糰'
+        },
+        {
+          img: 'https://dinbendon.kento520.tw/assets/img/res/pic/4.jpg',
+          title: '池上飯包'
+        }
+      ]
+    }
+  },
+  components: {
+    Card
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+// 元件用css
+// h1
+//   color red
+</style>
---
+++ myvue/src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-img">
+      <img :src="data.img">
+    </div>
+    <h1>{{data.title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
