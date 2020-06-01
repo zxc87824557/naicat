bd204a7addf76cb06808fe1fe085c4ad7654bd89
zxc87824557 <zxc90919zxc@gmail.com>
2020-06-01 09:19:37 +0800

    番茄鐘更新

--- vue/20200526/pomodoro/src/main.js
+++ vue/20200526/pomodoro/src/main.js
@@ -9,6 +9,8 @@ import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import draggable from 'vuedraggable'
+import 'noto-sans-tc/noto_sans_tc_regular/css.css'
+import './style/style.styl'
 // 根據 icon 的種類引用
 import { faCheck, faPen, faTimes, faUndo, faSave, faPlay, faPause, faStepForward } from '@fortawesome/free-solid-svg-icons'
 
