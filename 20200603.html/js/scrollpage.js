ae2e1e79ba822b667106f39c6951fa8e94334e8b
zxc87824557 <zxc90919zxc@gmail.com>
2020-06-03 13:35:00 +0800

    navbar+modal

---
+++ 20200603.html/js/scrollpage.js
@@ -0,0 +1,9 @@
+// $('a[href^="#"]').on('click', function () {
+//   let $target = $(this.getAttribute('href'))
+//   if ($target.length) {
+//     event.preventDefault()
+//     $('html,body').stop().animate({
+//       scropllTop: $target.offset().top
+//     }, 2000, 'easeInoutBack')
+//   }
+// })
