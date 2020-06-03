399c8ac3eecf3ee4625de3ce53f990acd1f6a4ba
zxc87824557 <zxc90919zxc@gmail.com>
2020-06-03 16:38:20 +0800

    section02

--- 20200603.html/js/scrollpage.js
+++ 20200603.html/js/scrollpage.js
@@ -1,9 +1,9 @@
-// $('a[href^="#"]').on('click', function () {
-//   let $target = $(this.getAttribute('href'))
-//   if ($target.length) {
-//     event.preventDefault()
-//     $('html,body').stop().animate({
-//       scropllTop: $target.offset().top
-//     }, 2000, 'easeInoutBack')
-//   }
-// })
+$('a[href^="#"]').on('click', function (event) {
+  let $target = $(this.getAttribute('href'))
+  if ($target.length) {
+    event.preventDefault()
+    $('html,body').stop().animate({
+      scrollTop: $target.offset().top
+    }, 2000, 'easeInOutBack')
+  }
+})
